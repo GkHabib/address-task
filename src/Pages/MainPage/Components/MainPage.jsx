f0531e6d8e3c5465d19ee934bba60689270c1acc
gkhabib <h.ghadimi1998@gmail.com>
2020-11-25 20:32:24 +0330

    add basic dialog and dialog content

--- src/Pages/MainPage/Components/MainPage.jsx
+++ src/Pages/MainPage/Components/MainPage.jsx
@@ -1,25 +1,133 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Button,
+  Button, Grid, Dialog, DialogTitle, DialogContent, Typography, IconButton, TextField,
 } from '@material-ui/core';
+import {
+  Input as InputIcon,
+  Close as CloseIcon,
+} from '@material-ui/icons';
+import { isMobile } from 'react-device-detect';
 import useStyles from '../Styles';
+
+const steps = [
+  'submitMobileNumber',
+  'submitConfirmationCode',
+];
 
 const MainPage = (props) => {
   const classes = useStyles();
+  const [isDialogOpen, setIsDialogOpen] = useState(true);
+  const openDialog = () => setIsDialogOpen(true);
+  const closeDialog = () => setIsDialogOpen(false);
+  const [step, setStep] = useState(steps[0]);
+  const resetStep = () => setStep(steps[0]);
+  const goToNextStep = () => setStep(steps[1]);
+  const stepDataObject = {
+    submitMobileNumber: {
+      title: 'تایید شماره موبایل',
+      textFieldLabel: 'شماره‌تلفن همراه',
+      textFieldHelperText: 'لطفا شماره‌تلفن همراه خود را وارد نمایید',
+      buttonLabel: 'درخواست کد',
+      onClick: () => {
+        goToNextStep();
+      },
+    },
+    submitConfirmationCode: {
+      title: 'تایید کد احراز هویت',
+      textFieldLabel: 'کد احراز هویت ارسال‌شده',
+      textFieldHelperText: 'لطفا کد احراز هویت ارسال‌شده به شماره‌تلفن همراه وارد شده را وارد نمایید',
+      buttonLabel: 'تایید کد احراز هویت',
+      onClick: () => {
+        closeDialog();
+        resetStep();
+      },
+    },
+  };
   return (
-    <Button
-      onClick={() => console.log('Submitted')}
-      margin="normal"
-      size="large"
-      variant="text"
-      color="primary"
-      fullWidth
-      className={classes.loginButton}
-      // startIcon={<PodIcon viewBox="0 0 48 48" />}
+    <Grid
+      container
+      xs={12}
+      direction="row"
+      justify={isMobile ? 'center' : 'flex-end'}
+      alignItems={isMobile ? 'center' : 'flex-start'}
+      className={classes.root}
     >
-      {'ورود/ثبت‌نام'}
-    </Button>
+      <Grid
+        item
+        xl={2}
+        lg={3}
+        md={4}
+        xs={12}
+      >
+        <Button
+          onClick={() => {
+            openDialog();
+          }}
+          margin="normal"
+          size="large"
+          variant="text"
+          color="primary"
+          fullWidth
+          className={classes.loginButton}
+          startIcon={<InputIcon />}
+        >
+          {'ورود/ثبت‌نام'}
+        </Button>
+      </Grid>
+      <Dialog
+        open={isDialogOpen}
+        onClose={closeDialog}
+        maxWidth="md"
+        fullWidth
+      >
+        <DialogTitle classes={{ root: classes.dialogTitleContainer }}>
+          <Typography variant="h6"  className={classes.dialogTitle}>
+            {stepDataObject[step].title}
+          </Typography>
+          <IconButton
+            className={classes.closeButton}
+            onClick={closeDialog}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <Grid
+            container
+            xs={12}
+            direction="column"
+            justify="center"
+            alignItems="center"
+            className={classes.dialogContentContainer}
+          >
+            <Grid container xs={12} md={6} style={{ margin: '16px 0' }}>
+              <TextField
+                fullWidth
+                label={stepDataObject[step].textFieldLabel}
+                variant="outlined"
+                helperText={stepDataObject[step].textFieldHelperText}
+                autoFocus
+              />
+            </Grid>
+            <Grid container xs={12} md={6} style={{ margin: '16px 0' }}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={stepDataObject[step].onClick}
+              >
+                {stepDataObject[step].buttonLabel}
+              </Button>
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
+    </Grid>
   );
+};
+
+MainPage.propType = {
+
 };
 
 
--- src/Pages/MainPage/Styles/index.js
+++ src/Pages/MainPage/Styles/index.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100vh',
-    paddingTop: '15vh',
+    padding: '4% 2%',
   },
   loginButton: {
     backgroundColor: '#212121',
@@ -10,6 +10,28 @@ const useStyles = makeStyles(theme => ({
     '&:hover': {
       backgroundColor: '#424242',
     },
+    borderRadius: '1rem',
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  dialogTitle: {
+    position: 'absolute',
+    left: theme.spacing(2),
+    top: theme.spacing(2),
+  },
+  dialogTitleContainer: {
+    position: 'relative',
+  },
+  dialogSubmitButton: {
+    height: '36px',
+    width: '100px',
+  },
+  dialogContentContainer: {
+    padding: theme.spacing(4, 2),
   },
 }));
 
